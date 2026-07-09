4e50100cb72e54a7520fc0654f6f7337eb344c65
Sai Chandini <r.saichandini@gmail.com>
2026-07-09 17:24:21 +0530

    Hands-on 1: Created Spring Boot Maven project

---
+++ src/test/java/com/example/springboothandson/SpringBootHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHandsOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
