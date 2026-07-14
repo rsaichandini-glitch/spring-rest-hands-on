269bb93e1d58deaa9ca6de0592d535435054d97d
Sai Chandini <r.saichandini@gmail.com>
2026-07-14 14:30:33 +0530

    Added Handson-2 - Hello World RESTful Web Service

---
+++ Handson-2/src/main/java/com/cognizant/handson2/Handson2Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.handson2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Handson2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Handson2Application.class, args);
+    }
+
+}
