066cebe38d6efb65e94b689480bb0c17f96f44d0
Sai Chandini <r.saichandini@gmail.com>
2026-07-14 14:44:52 +0530

    Added Handson-3 - REST Country Web Service

---
+++ Handson-3/src/main/java/com/cognizant/handson3/Handson3Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.handson3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Handson3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Handson3Application.class, args);
+    }
+
+}
---
+++ Handson-3/src/main/java/controller/CountryController.java
@@ -0,0 +1,14 @@
+package com.cognizant.handson3.controller;
+
+import com.cognizant.handson3.model.Country;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/country")
+    public Country getCountry() {
+        return new Country("IN", "India");
+    }
+}
